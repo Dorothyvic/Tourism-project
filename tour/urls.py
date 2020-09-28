cbd29766d655ff2325987898005589eae6ff7e5c
Nahabwe Edwin <34930823+musicmeetscode@users.noreply.github.com>
2020-09-28 11:18:37 +0300

    Removed pycache from track list

--- tour/urls.py
+++ tour/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='index'),
     path('home/', views.home, name='home'),
     path('popular_tours/', views.tour, name='tours'),
     path('stories/', views.story, name='stories'),
--- tourism/urls.py
+++ tourism/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tourism/', include('tour.urls')),
+    path('', include('tour.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
